9cfde1b2ce831fc17d5fe6e0a1a0311db7da1580
Beau Simensen <beau@dflydev.com>
2012-11-26 15:36:46 -0800

    Added Textile support.

---
+++ src/Sculpin/Bundle/TextileBundle/SculpinTextileBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sculpin\Bundle\TextileBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Sculpin Textile Bundle.
+ *
+ * @author Beau Simensen <beau@dflydev.com>
+ */
+class SculpinTextileBundle extends Bundle
+{
+    const CONVERTER_NAME = 'textile';
+}
