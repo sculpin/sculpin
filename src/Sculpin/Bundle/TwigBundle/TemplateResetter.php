55ed29665b85069a4fd1cd7fd51b548f808bb04e
Beau Simensen <beau@dflydev.com>
2012-12-05 14:28:55 -0800

    Call clearTemplateCache anytime changes have been made
    Thanks @igorw
    
    Closes #53

---
+++ src/Sculpin/Bundle/TwigBundle/TemplateResetter.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sculpin\Bundle\TwigBundle;
+
+use Sculpin\Core\Event\SourceSetEvent;
+use Sculpin\Core\Sculpin;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Template Resetter.
+ *
+ * @author Beau Simensen <beau@dflydev.com>
+ */
+class TemplateResetter implements EventSubscriberInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param Twig_Environment $twig
+     */
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Sculpin::EVENT_BEFORE_RUN => 'beforeRun',
+        );
+    }
+
+    /**
+     * Before run
+     *
+     * @param SourceSetEvent $sourceSetEvent Source Set Event
+     */
+    public function beforeRun(SourceSetEvent $sourceSetEvent)
+    {
+        if ($sourceSetEvent->updatedSources()) {
+            $this->twig->clearTemplateCache();
+        }
+    }
+}
