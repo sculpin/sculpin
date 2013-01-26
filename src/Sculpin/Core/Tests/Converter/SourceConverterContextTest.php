e96da470ee75288b262d12661092efa8350041f0
Juan Treminio <jtreminio@gmail.com>
2013-01-25 23:40:57 -0600

    phpunit-tests - Extending base class; using getMockBuilder; removed unneeded test; changed method names to be more descriptive

--- src/Sculpin/Core/Tests/Converter/SourceConverterContextTest.php
+++ src/Sculpin/Core/Tests/Converter/SourceConverterContextTest.php
@@ -11,14 +11,20 @@
 
 namespace Sculpin\Core\Tests\Converter;
 
+use Sculpin\Core\Tests\Base;
 use Sculpin\Core\Converter\SourceConverterContext;
 use Sculpin\Core\Source\SourceInterface;
+use \PHPUnit_Framework_MockObject_MockObject;
 
-class SourceConverterContextTest extends \PHPUnit_Framework_TestCase
+class SourceConverterContextTest extends Base
 {
-    public function testContent()
+    public function testSourceConverterContextReturnsExpectedOutput()
     {
-        $source = $this->getMock('Sculpin\Core\Source\SourceInterface');
+        /** @var $source PHPUnit_Framework_MockObject_MockObject|SourceInterface */
+        $source = $this
+            ->getMockBuilder('Sculpin\Core\Source\SourceInterface')
+            ->getMock();
+
         $source
             ->expects($this->once())
             ->method('content')
@@ -27,17 +33,5 @@ class SourceConverterContextTest extends \PHPUnit_Framework_TestCase
         $sourceConverterContext = new SourceConverterContext($source);
 
         $this->assertEquals('hello world', $sourceConverterContext->content());
-    }
-
-    public function testSetContent()
-    {
-        $source = $this->getMock('Sculpin\Core\Source\SourceInterface');
-        $source
-            ->expects($this->once())
-            ->method('setContent')
-            ->with($this->equalTo('hello world'));
-
-        $sourceConverterContext = new SourceConverterContext($source);
-        $response = $sourceConverterContext->setContent('hello world');
     }
 }
