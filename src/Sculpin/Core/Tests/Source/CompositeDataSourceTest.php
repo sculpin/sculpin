f712e68e28d2ca159d85a12433ef8b5713967035
Juan Treminio <jtreminio@gmail.com>
2013-01-25 23:42:53 -0600

    phpunit-tests - Extending Base; using getMockBuilder; better method names; formatting

--- src/Sculpin/Core/Tests/Source/CompositeDataSourceTest.php
+++ src/Sculpin/Core/Tests/Source/CompositeDataSourceTest.php
@@ -13,12 +13,21 @@ namespace Sculpin\Core\Tests\Formatter;
 
 use Sculpin\Core\Source\CompositeDataSource;
 use Sculpin\Core\Source\SourceSet;
+use Sculpin\Core\Source\DataSourceInterface;
+use Sculpin\Core\Tests\Base;
+use \PHPUnit_Framework_MockObject_MockObject;
 
-class CompositeDataSourceTest extends \PHPUnit_Framework_TestCase
+class CompositeDataSourceTest extends Base
 {
+    /**
+     * @param string $dataSourceId
+     * @return PHPUnit_Framework_MockObject_MockObject|DataSourceInterface
+     */
     public function makeDataSource($dataSourceId)
     {
-        $dataSource = $this->getMock('Sculpin\Core\Source\DataSourceInterface');
+        $dataSource = $this
+            ->getMockBuilder('Sculpin\Core\Source\DataSourceInterface')
+            ->getMock();
 
         $dataSource
             ->expects($this->any())
@@ -28,7 +37,7 @@ class CompositeDataSourceTest extends \PHPUnit_Framework_TestCase
         return $dataSource;
     }
 
-    public function testAddDataSource()
+    public function testAddDataSourceSetsDataSourcesAsArray()
     {
         $ds000 = $this->makeDataSource('TestDataSource:000');
         $ds001 = $this->makeDataSource('TestDataSource:001');
@@ -36,21 +45,27 @@ class CompositeDataSourceTest extends \PHPUnit_Framework_TestCase
 
         $dataSource = new CompositeDataSource(array($ds000, $ds002));
 
-        $this->assertEquals(array(
-            'TestDataSource:000' => $ds000,
-            'TestDataSource:002' => $ds002,
-        ), $dataSource->dataSources());
+        $this->assertEquals(
+            array(
+                'TestDataSource:000' => $ds000,
+                'TestDataSource:002' => $ds002,
+            ),
+            $dataSource->dataSources()
+        );
 
         $dataSource->addDataSource($ds001);
 
-        $this->assertEquals(array(
-            'TestDataSource:000' => $ds000,
-            'TestDataSource:002' => $ds002,
-            'TestDataSource:001' => $ds001,
-        ), $dataSource->dataSources());
+        $this->assertEquals(
+            array(
+                'TestDataSource:000' => $ds000,
+                'TestDataSource:002' => $ds002,
+                'TestDataSource:001' => $ds001,
+            ),
+            $dataSource->dataSources()
+        );
     }
 
-    public function testDataSourceId()
+    public function testCompositeDataSourceSetsDataSourceId()
     {
         $ds000 = $this->makeDataSource('TestDataSource:000');
         $ds001 = $this->makeDataSource('TestDataSource:001');
