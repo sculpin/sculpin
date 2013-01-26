2dfcd424d82e2e21700c916de981710fcd1199dd
Juan Treminio <jtreminio@gmail.com>
2013-01-25 23:39:00 -0600

    phpunit-tests - Added base PHPUnit class for all classes to extend. Will make it easier to add suite-wide functionality in the future

---
+++ src/Sculpin/Core/Tests/Base.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sculpin\Core\Tests;
+
+class Base extends \PHPUnit_Framework_TestCase
+{
+}
