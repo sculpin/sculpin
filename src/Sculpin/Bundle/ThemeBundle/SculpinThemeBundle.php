aede1311cb5b724de8db00bd983e4ed6e382c2aa
Beau Simensen <beau@dflydev.com>
2013-12-15 01:16:11 -0600

    Theme
    I wrote a very long commit message with a lot of notes and vim on mac lost them
    all. Awesome.
    
    refs #85

---
+++ src/Sculpin/Bundle/ThemeBundle/SculpinThemeBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sculpin\Bundle\ThemeBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Sculpin Theme Bundle.
+ *
+ * @author Beau Simensen <beau@dflydev.com>
+ */
+class SculpinThemeBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
