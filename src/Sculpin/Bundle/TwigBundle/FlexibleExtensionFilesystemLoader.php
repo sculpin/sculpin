sculpin/sculpin

commit f177cdc61c150732d978887992e54c1fb1792661

Optimizing the Flexible Extension Filesystem Loader
This addresses a pretty big performance problem that exploded by supporting many
more standard directories. Twig was needing to look in many locations over and
over again for the same templates.

src/Sculpin/Bundle/TwigBundle/FlexibleExtensionFilesystemLoader.php
@@ -11,12 +11,16 @@
 
 namespace Sculpin\Bundle\TwigBundle;
 
+use Sculpin\Core\Event\SourceSetEvent;
+use Sculpin\Core\Sculpin;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
 /**
  * Flexible Extension Filesystem Loader.
  *
  * @author Beau Simensen <beau@dflydev.com>
  */
-class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface
+class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface, EventSubscriberInterface
 {
     /**
      * Filesystem loader
@@ -24,6 +28,10 @@ class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface
      * @var FilesystemLoader
      */
     protected $filesystemLoader;
+
+    protected $cachedCacheKey = array();
+    protected $cachedCacheKeyExtension = array();
+    protected $cachedCacheKeyException = array();
 
     /**
      * Constructor.
@@ -61,13 +69,11 @@ class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface
      */
     public function getSource($name)
     {
-        foreach ($this->extensions as $extension) {
-            try {
-                return $this->filesystemLoader->getSource($name.$extension);
-            } catch (\Twig_Error_Loader $e) {
-            }
-        }
-        throw new \Twig_Error_Loader(sprintf('Template "%s" is not defined.', $name));
+        $this->getCacheKey($name);
+
+        $extension = $this->cachedCacheKeyExtension[$name];
+
+        return $this->filesystemLoader->getSource($name.$extension);
     }
 
     /**
@@ -79,13 +85,27 @@ class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface
      */
     public function getCacheKey($name)
     {
+        if (isset($this->cachedCacheKey[$name])) {
+            return $this->cachedCacheKey[$name];
+        }
+
+        if (isset($this->cachedCacheKeyException[$name])) {
+            throw $this->cachedCacheKeyException[$name];
+        }
+
         foreach ($this->extensions as $extension) {
             try {
-                return $this->filesystemLoader->getCacheKey($name.$extension);
+                $this->cachedCacheKey[$name] = $this->filesystemLoader->getCacheKey($name.$extension);
+                $this->cachedCacheKeyExtension[$name] = $extension;
+
+                return $this->cachedCacheKey[$name];
             } catch (\Twig_Error_Loader $e) {
             }
         }
-        throw new \Twig_Error_Loader(sprintf('Template "%s" is not defined.', $name));
+
+        throw $this->cachedCacheKeyException[$name] = new \Twig_Error_Loader(
+            sprintf('Template "%s" is not defined.', $name)
+        );
     }
 
     /**
@@ -98,12 +118,29 @@ class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface
      */
     public function isFresh($name, $time)
     {
-        foreach ($this->extensions as $extension) {
-            try {
-                return $this->filesystemLoader->isFresh($name.$extension, $time);
-            } catch (\Twig_Error_Loader $e) {
-            }
+        $this->getCacheKey($name);
+
+        $extension = $this->cachedCacheKeyExtension[$name];
+
+        return $this->filesystemLoader->isFresh($name.$extension, $time);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Sculpin::EVENT_BEFORE_RUN => 'beforeRun',
+        );
+    }
+
+    public function beforeRun(SourceSetEvent $sourceSetEvent)
+    {
+        if ($sourceSetEvent->sourceSet()->newSources()) {
+            $this->cachedCacheKey = array();
+            $this->cachedCacheKeyExtension = array();
+            $this->cachedCacheKeyException = array();
         }
-        throw new \Twig_Error_Loader(sprintf('Template "%s" is not defined.', $name));
     }
 }

src/Sculpin/Bundle/TwigBundle/Resources/config/services.xml
@@ -21,6 +21,7 @@
             <argument>%sculpin_twig.source_view_paths%</argument>
             <argument>%sculpin_twig.view_paths%</argument>
             <argument>%sculpin_twig.extensions%</argument>
+            <tag name="kernel.event_subscriber" />
             <tag name="sculpin.path_configurator" type="exclude" parameter="sculpin_twig.source_view_paths" />
             <tag name="sculpin.custom_mime_extensions" type="text/twig" parameter="sculpin_twig.extensions" />
         </service>
@@ -31,8 +32,8 @@
 
         <service id="sculpin_twig.loader" class="%sculpin_twig.loader.class%">
             <argument type="collection">
-                <argument type="service" id="sculpin_twig.flexible_extension_filesystem_loader" />
                 <argument type="service" id="sculpin_twig.array_loader" />
+                <argument type="service" id="sculpin_twig.flexible_extension_filesystem_loader" />
             </argument>
         </service>
 

src/Sculpin/Bundle/TwigBundle/TwigFormatter.php
@@ -75,9 +75,14 @@ class TwigFormatter implements FormatterInterface
     public function formatPage(FormatContext $formatContext)
     {
         try {
-            $this->arrayLoader->setTemplate($formatContext->templateId(), $this->massageTemplate($formatContext));
+            $this->arrayLoader->setTemplate(
+                $formatContext->templateId(),
+                $this->massageTemplate($formatContext)
+            );
 
-            return $this->twig->render($formatContext->templateId(), $formatContext->data()->export());
+            $data = $formatContext->data()->export();
+
+            return $this->twig->render($formatContext->templateId(), $data);
         } catch (Exception $e) {
             print " [ exception ]\n";
         }

src/Sculpin/Core/Source/SourceSet.php
@@ -26,6 +26,13 @@ class SourceSet
     protected $sources = array();
 
     /**
+     * New Sources
+     *
+     * @var Array
+     */
+    protected $newSources = array();
+
+    /**
      * Constructor.
      *
      * @param Array $sources
@@ -57,6 +64,8 @@ class SourceSet
     {
         if (array_key_exists($source->sourceId(), $this->sources)) {
             unset($this->sources[$source->sourceId()]);
+        } else {
+            $this->newSources[$source->sourceId()] = $source;
         }
         $this->sources[$source->sourceId()] = $source;
     }
@@ -83,6 +92,11 @@ class SourceSet
         });
     }
 
+    public function newSources()
+    {
+        return $this->newSources;
+    }
+
     /**
      * Reset all sources
      *
@@ -93,5 +107,7 @@ class SourceSet
         foreach ($this->sources as $source) {
             $source->setHasNotChanged();
         }
+
+        $this->newSources = array();
     }
 }

commit 64c359fd034f0598ede076b696d7a640507a8898

Fixed a bug introduced in f177cdc61c150732d978887992e54c1fb1792661

src/Sculpin/Bundle/TwigBundle/FlexibleExtensionFilesystemLoader.php
@@ -86,7 +86,9 @@ class FlexibleExtensionFilesystemLoader implements \Twig_LoaderInterface, EventS
     public function getCacheKey($name)
     {
         if (isset($this->cachedCacheKey[$name])) {
-            return $this->cachedCacheKey[$name];
+            $extension = $this->cachedCacheKeyExtension[$name];
+
+            return $this->cachedCacheKey[$name] = $this->filesystemLoader->getCacheKey($name.$extension);
         }
 
         if (isset($this->cachedCacheKeyException[$name])) {