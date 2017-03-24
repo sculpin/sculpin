sculpin/sculpin

commit dfdf629da25a095c93a229e2c240ec034771081d

Fix issue #179 while keeping compability.

src/Sculpin/Core/Permalink/SourcePermalinkFactory.php
@@ -122,6 +122,11 @@ class SourcePermalinkFactory implements SourcePermalinkFactoryInterface
                 $prettyBasename = false !== strrpos($basename, '.') ? substr($basename, 0, strrpos($basename, '.')) : $basename;
                 $permalink = preg_replace('/:basename_real/', $basename, $permalink);
                 $permalink = preg_replace('/:basename/', $prettyBasename, $permalink);
+
+                if (substr($permalink, -5, 5) != '.html') {
+                    $permalink = rtrim($permalink, '/').'/';
+                }
+
                 if (substr($permalink, -1, 1) == '/') {
                     $permalink .= 'index.html';
                 }

src/Sculpin/Core/Tests/Permalink/SourcePermalinkFactoryTest.php
@@ -65,6 +65,60 @@ class SourcePermalinkFactoryTest extends \PHPUnit_Framework_TestCase
                     '/2015/01/12/from-buttercup-protects-to-broadway/'
                 ),
             ),
+
+            array(
+                'blog/:year/:month/:day/:slug_title',
+                static::makeTestSource('about.md', array('slug' => 'some-about-me', 'calculated_date' => mktime(0, 0, 0, 1, 12, 2005))),
+                new Permalink(
+                    'blog/2005/01/12/some-about-me/index.html',
+                    '/blog/2005/01/12/some-about-me/'
+                ),
+            ),
+
+            array(
+                ':basename.html/',
+                static::makeTestSource('about.md'),
+                new Permalink(
+                    'about.html/index.html',
+                    '/about.html/'
+                ),
+            ),
+
+            array(
+                ':filename.html',
+                static::makeTestSource('about.md'),
+                new Permalink(
+                    'about.md.html',
+                    '/about.md.html'
+                ),
+            ),
+
+            array(
+                ':filename.html/',
+                static::makeTestSource('about.md'),
+                new Permalink(
+                    'about.md.html/index.html',
+                    '/about.md.html/'
+                ),
+            ),
+
+            array(
+                ':filename',
+                static::makeTestSource('about.md'),
+                new Permalink(
+                    'about.md/index.html',
+                    '/about.md/'
+                ),
+            ),
+
+            array(
+                ':filename/',
+                static::makeTestSource('about.md'),
+                new Permalink(
+                    'about.md/index.html',
+                    '/about.md/'
+                ),
+            ),
         );
     }
 

commit 4cc71e733ac43c2fbb4da0567e13265a587fb5f3

Fix regression introduced by #233

src/Sculpin/Core/Permalink/SourcePermalinkFactory.php
@@ -133,8 +133,12 @@ class SourcePermalinkFactory implements SourcePermalinkFactoryInterface
                 $permalink = preg_replace('/:basename_real/', $basename, $permalink);
                 $permalink = preg_replace('/:basename/', $prettyBasename, $permalink);
 
-                if (substr($permalink, -5, 5) != '.html') {
-                    $permalink = rtrim($permalink, '/').'/';
+                if (preg_match('#(^|[\\/])[^.]+$#', $permalink)
+                    // Exclude .md and .twig for BC
+                    || substr($permalink, -3, 3) === '.md'
+                    || substr($permalink, -5, 5) === '.twig'
+                ) {
+                    $permalink = rtrim($permalink, '/') . '/';
                 }
 
                 if (substr($permalink, -1, 1) == '/') {

src/Sculpin/Core/Tests/Permalink/SourcePermalinkFactoryTest.php
@@ -131,6 +131,24 @@ class SourcePermalinkFactoryTest extends \PHPUnit_Framework_TestCase
                 ),
             ),
 
+            'Permalink for .xml' => array(
+                ':filename',
+                static::makeTestSource('about.xml'),
+                new Permalink(
+                    'about.xml',
+                    '/about.xml'
+                ),
+            ),
+
+            'Permalink for .json' => array(
+                ':filename',
+                static::makeTestSource('about.json'),
+                new Permalink(
+                    'about.json',
+                    '/about.json'
+                ),
+            ),
+
             'Permalink with trailing slash' => array(
                 ':basename/',
                 static::makeTestSource('about.md'),