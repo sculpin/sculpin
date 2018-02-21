42526e850b46e2264ddbd1cb7e7ea0b449da9f4c
Kevin Boyd <kevin.boyd@gmail.com>
2018-02-21 00:18:01 -0800

    Use reflection to bring back Twig template cache invalidation

---
+++ src/Sculpin/Bundle/TwigBundle/Environment.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of Sculpin.
+ *
+ * (c) Dragonfly Development Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sculpin\Bundle\TwigBundle;
+
+/**
+ * Twig Environment with Loaded Template Invalidation.
+ */
+class Environment extends \Twig_Environment
+{
+    /**
+     * We aren't supposed to be using loadTemplate directly, and Twig V2 took away our ability to invalidate it.
+     *
+     * Hacky workaround to expose invalidation.
+     */
+    public function invalidateLoadedTemplates() {
+        try {
+            $me     = new \ReflectionClass($this);
+            $parent = $me;
+
+            while ($parent !== false) {
+                $me     = $parent;
+                $parent = $me->getParentClass();
+            }
+
+            $templates = $me->getProperty('loadedTemplates');
+            $templates->setAccessible(true);
+            $templates->setValue($this, []);
+        } catch (\ReflectionException $e) {}
+    }
+}
