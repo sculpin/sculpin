233927b3bff9d5750ccc459531d9e487c6062161
Kevin Boyd <kevin.boyd@gmail.com>
2019-04-24 23:57:35 -0700

    Update the visual style and listener behaviour

--- src/Sculpin/Bundle/SculpinBundle/Resources/js/editor.js
+++ src/Sculpin/Bundle/SculpinBundle/Resources/js/editor.js
@@ -8,12 +8,15 @@ var SculpinEditor = {
         var body = document.getElementsByTagName('body')[0];
 
         // render bottom bar with edit button
-        body.innerHTML += '<div style="" id="SCULPIN_BOTTOM_BAR"><BUTTON id="SCULPIN_EDIT_BUTTON">Edit This Page</BUTTON></div>';
-
-        // create button listener
-        document.getElementById("SCULPIN_EDIT_BUTTON").addEventListener('click', function () {
-            SculpinEditor.renderEditor();
-        });
+        body.innerHTML += '<div style="background-color: steelblue; padding: 20px; margin: 0; position: absolute; bottom: 0; left: 0; right: 0;" id="SCULPIN_BOTTOM_BAR">' +
+            '<div style="float: left; color: white; font-family: sans-serif;font-size: 1.1em;"><strong><em style="text-transform: uppercase; padding-right: 20px;">Sculpin</em></strong>' +
+            '<a href="https://sculpin.io/documentation/sources" style="color: white;">Documentation</a></div>' +
+            '<div style="float: right;">' +
+            '<span style="color: white; font-family: sans-serif; padding-right: 20px;"><strong>Current Disk Path:</strong> /'+SCULPIN_EDITOR_METADATA.diskPath+'</span>' +
+            '<BUTTON id="SCULPIN_EDIT_BUTTON" style="color: #ffffff; background-color: #9f1770; border: 0; padding: 10px; font-weight: bolder;">Edit This Page</BUTTON></div>' +
+            '</div>';
+        // width: 100%; background-color: steelblue; padding-top: 20px; padding-bottom: 20px; position: fixed; bottom: 0px; left: 0px; padding-left: 0px; margin: 0px !important; display: none;
+        this.registerListeners();
     },
 
     renderEditor: function () {
@@ -22,11 +25,49 @@ var SculpinEditor = {
 
         // load editor box with content
         var body = document.getElementsByTagName('body')[0];
-        body.innerHTML += '<textarea style="width: 100%; height: 100%; font-size: 1.02em; font-family: \'Roboto Mono\', monospace;" id="SCULPIN_EDIT_TEXTAREA">' + SCULPIN_EDITOR_METADATA.content + '</textarea><button id="SCULPIN_SAVE_CHANGES">Save Changes</button>';
+        body.innerHTML += '<div id="SCULPIN_EDIT_PANEL" style="background-color: steelblue; padding: 12px; position: fixed; bottom: 0; height: 50%; left: 0; right: 0;">' +
+            '<h3 style="color: white; font-family: sans-serif;">Editing <small>/'+SCULPIN_EDITOR_METADATA.diskPath+'</small></h3>' +
+            '<textarea style="width: 100%; font-size: 1.02em; font-family: \'Roboto Mono\', monospace; padding-bottom: 12px; height: 70%;" id="SCULPIN_EDIT_TEXTAREA">' + SCULPIN_EDITOR_METADATA.content + '</textarea>' +
+            '<div style="margin-top: 8px;"><button id="SCULPIN_SAVE_CHANGES" style="color: #ffffff; background-color: #9f1770; border: 0; padding: 10px; margin-right: 12px;font-weight: bolder;">Save Changes</button>' +
+            '<button id="SCULPIN_CANCEL_CHANGES" style="color: #ffffff; background-color: #9f480c; border: 0; padding: 10px; margin-top: 10px;font-weight: bolder;">Cancel</button></div>' +
+            '</div>';
+
+        this.registerListeners();
+    },
+
+    registerListeners: function () {
+        let editButton = document.getElementById("SCULPIN_EDIT_BUTTON");
+        let saveButton = document.getElementById("SCULPIN_SAVE_CHANGES");
+        let cancelButton = document.getElementById("SCULPIN_CANCEL_CHANGES");
+
+        editButton && editButton.addEventListener('click', function () {
+            var editor = document.getElementById('SCULPIN_EDIT_PANEL');
+            var menuBar = document.getElementById('SCULPIN_BOTTOM_BAR');
+
+            if (editor && editor.length > 0) {
+                editor.style.display = 'block';
+                menuBar.style.display = 'none';
+
+                SculpinEditor.registerListeners();
+
+                return;
+            }
+
+            SculpinEditor.renderEditor();
+        });
 
         // create save button listener
-        document.getElementById("SCULPIN_SAVE_CHANGES").addEventListener('click', function () {
+
+        saveButton && saveButton.addEventListener('click', function () {
             SculpinEditor.saveChanges();
+        });
+
+        cancelButton && cancelButton.addEventListener('click', function () {
+            // @todo check if the content has changed and ask the user to confirm if they want to discard their changes
+            document.getElementById('SCULPIN_BOTTOM_BAR').style.display = 'block';
+            document.getElementById('SCULPIN_EDIT_PANEL').style.display = 'none';
+
+            SculpinEditor.registerListeners();
         });
     },
 
