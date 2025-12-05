6870936411d6d8c68b3d7eaa1db049a1bd05807b
Kevin Boyd <kevin.boyd@gmail.com>
2025-12-04 21:36:57 -0800

    Fix a fatal in non-editor mode.

--- src/Sculpin/Bundle/SculpinBundle/HttpServer/ContentFetcher.php
+++ src/Sculpin/Bundle/SculpinBundle/HttpServer/ContentFetcher.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Sculpin\Bundle\SculpinBundle\HttpServer;
 
+use Sculpin\Core\Source\SourceSet;
+
 interface ContentFetcher
 {
     public function fetchData(string $path): ?string;
+    public function buildPathMap(SourceSet $set): void;
 }
--- src/Sculpin/Bundle/SculpinBundle/HttpServer/DefaultContentFetcher.php
+++ src/Sculpin/Bundle/SculpinBundle/HttpServer/DefaultContentFetcher.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Sculpin\Bundle\SculpinBundle\HttpServer;
 
+use Sculpin\Core\Source\SourceSet;
+
 class DefaultContentFetcher implements ContentFetcher
 {
     public function fetchData(string $path): ?string
     {
         return file_get_contents($path) ?: null;
+    }
+
+    public function buildPathMap(SourceSet $set): void
+    {
+        // Nothing to do here.
     }
 }
